e352309e39121e14b224f558f56301f51aa83422
Mariah Cooperwood <lanelle@umich.edu>
2026-01-28 22:07:53 -0500

    Added email

--- HW3.py
+++ HW3.py
@@ -1,6 +1,6 @@
 # Name: Mariah Cooperwood
 # Student ID: 32843209
-# Email:
+# Email: lanelle@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
 # e.g.:
